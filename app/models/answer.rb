cd48974a2b237ff333fe618e66e1aaf22dc8ae22
ark <a.shraer@gmail.com>
2019-09-22 16:49:01 +0300

    added scopes

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :user
+
+  # returns hash of number correct and incorrect answers, e.g. { true => 2, false => 3 }
+  scope :accuracy, -> { group(:correct).count }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   belongs_to :timezone
+
+  # scope which gets answers for user between given dates and respecting user's timezone
   has_many :answers do
-    def date_range
-      # scope :date_range, ->(date_from, date_to, timezone) { where(created_at: DateTime.parse(date_from)..DateTime.parse(date_to)) }
+    def in_range(from_date, to_date)
+      where("convert_tz(created_at, 'UTC', '#{@association.owner.timezone}') BETWEEN '#{from_date}' AND '#{to_date}'")
     end
   end
 end
